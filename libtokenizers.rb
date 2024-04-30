8d03922f0fa440eec2fc7b7d785c6e59434002ed
Junji Hashimoto <junji.hashimoto@gree.net>
2024-04-30 12:34:20 +0900

    Change x86_64 to arm64

--- libtokenizers.rb
+++ libtokenizers.rb
@@ -1,8 +1,8 @@
 class Libtokenizers < Formula
   desc "libtokenizers for haskell-bindings"
   homepage "https://github.com/hasktorch/tokenizers"
-  url "https://github.com/hasktorch/tokenizers/releases/download/libtokenizers-v0.1/libtokenizers-macos.zip"
-  sha256 "a87ec816bdfe8acebca077d1ac51823ed7efb1d309047dbd98345c26d47accea"
+  url "https://github.com/hasktorch/tokenizers/releases/download/libtokenizers-v0.1/libtokenizers-macos-arm64.zip"
+  sha256 "4700b99f07b5cc7c11d8d441d4070639222db9db729221afe38286ad8bcc6b98"
   license "Apache-2.0"
 
   def install
