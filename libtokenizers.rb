e126f89955e144ae6d854d20ca7c36a1f133c494
Junji Hashimoto <junji.hashimoto@gree.net>
2024-05-05 04:58:02 +0900

    Fix the paths of both libc++ and libiconv

--- libtokenizers.rb
+++ libtokenizers.rb
@@ -2,12 +2,11 @@ class Libtokenizers < Formula
   desc "libtokenizers for haskell-bindings"
   homepage "https://github.com/hasktorch/tokenizers"
   url "https://github.com/hasktorch/tokenizers/releases/download/libtokenizers-v0.1/libtokenizers-macos-arm64.zip"
-  sha256 "4700b99f07b5cc7c11d8d441d4070639222db9db729221afe38286ad8bcc6b98"
+  sha256 "83e0f74daea43e5cbda80b32edacfed6ce9bec1037fc7fe413690e332777626d"
   license "Apache-2.0"
 
   def install
     system "bash", "-c", "install -d #{lib}"
     system "bash", "-c", "cp -a lib/* #{lib}"
-    system "bash", "-c", "chmod +x #{lib}/*"
   end
 end
